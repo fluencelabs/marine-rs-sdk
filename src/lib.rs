f800a0430bef3e51756d97bac76103785921d681
vms <michail.vms@gmail.com>
2020-07-28 18:44:24 +0300

    fix doc

--- src/lib.rs
+++ src/lib.rs
@@ -17,8 +17,8 @@
 //! Rust backend SDK for applications on the Fluence network. This crate defines the procedure macro
 //! `#[fce]` that could be applied to a function, structure or extern block.
 //!
-//! Structures with `#[fce]` could be used then in function arguments and values
-//! (hereinafter they'll be called records). All fields of a record should be public and have one of the
+//! Structures with `#[fce]` (hereinafter they'll be called records) could be used then in function
+//! arguments and values. All fields of a record should be public and have one of the
 //! following primitive Rust types
 //! (`bool, u8, u16, u32, u64, i8, i16, i32, i64, f32, f64, String, Vec<u8>`).
 //! ```rust
@@ -26,8 +26,8 @@
 //!
 //! #[fce]
 //! struct T {
-//!     field_1: i32,
-//!     field_2: Vec<u8>,
+//!     pub field_1: i32,
+//!     pub field_2: Vec<u8>,
 //! }
 //! ```
 //!
