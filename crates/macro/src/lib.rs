8d427c0e72e978922abbd1da0157b58c6a16a42f
vms <michail.vms@gmail.com>
2021-01-12 11:26:39 +0300

    catch up crates.io version

--- crates/macro/src/lib.rs
+++ crates/macro/src/lib.rs
@@ -54,7 +54,7 @@
 //!
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/fluence-sdk-macro/0.2.14")]
+#![doc(html_root_url = "https://docs.rs/fluence-sdk-macro/0.2.18")]
 #![deny(
     dead_code,
     nonstandard_style,
--- src/lib.rs
+++ src/lib.rs
@@ -55,7 +55,7 @@
 //!     pub fn curl_get(url: String) -> String;
 //! }
 //! ```
-#![doc(html_root_url = "https://docs.rs/fluence/0.2.14")]
+#![doc(html_root_url = "https://docs.rs/fluence/0.2.18")]
 #![deny(
     dead_code,
     nonstandard_style,
