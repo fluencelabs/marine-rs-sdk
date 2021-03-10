d6521af102077f03644888ea8c92290e9e952caa
vms <michail.vms@gmail.com>
2021-03-11 00:07:06 +0300

    simplify the code

--- src/lib.rs
+++ src/lib.rs
@@ -92,15 +92,16 @@ pub mod internal {
     pub use fluence_sdk_main::set_result_size;
 }
 
-const VERSION_SIZE: usize = env!("CARGO_PKG_VERSION").len();
+const PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
+const VERSION_SIZE: usize = PKG_VERSION.len();
 
 const fn sdk_version() -> [u8; VERSION_SIZE] {
-    const VERSION_AS_STR: &[u8] = env!("CARGO_PKG_VERSION").as_bytes();
+    let version_as_str = PKG_VERSION.as_bytes();
 
     let mut version_as_array: [u8; VERSION_SIZE] = [0; VERSION_SIZE];
     let mut byte_id = 0;
     while byte_id < VERSION_SIZE {
-        version_as_array[byte_id] = VERSION_AS_STR[byte_id];
+        version_as_array[byte_id] = version_as_str[byte_id];
         byte_id += 1;
     }
 
