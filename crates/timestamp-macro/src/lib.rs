22c493b8fd119d9b85e47b0784f5dec4c956663a
vms <michail.vms@gmail.com>
2021-04-08 13:36:45 +0300

    fmt

--- crates/timestamp-macro/src/lib.rs
+++ crates/timestamp-macro/src/lib.rs
@@ -28,11 +28,10 @@
 
 use proc_macro::TokenStream;
 
-
 #[proc_macro]
 pub fn build_timestamp(_: TokenStream) -> TokenStream {
     let current_utc_date = chrono::Utc::now();
-    let current_utc_date =  current_utc_date.to_rfc3339();
+    let current_utc_date = current_utc_date.to_rfc3339();
 
     let glue_code = quote::quote! { const __FCE_SDK_BUILD_TIME: &str = #current_utc_date; };
     glue_code.into()
