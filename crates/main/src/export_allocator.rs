85a93763c150b6f8f0f3d1e6a52dae78527f678a
vms <michail.vms@gmail.com>
2021-04-25 01:05:48 +0300

    add temp logs

--- crates/main/src/export_allocator.rs
+++ crates/main/src/export_allocator.rs
@@ -24,6 +24,11 @@ pub unsafe fn allocate(elem_count: usize, elem_ty: usize) -> usize {
     }
 
     let allocated_mem = allocate_impl(elem_count, elem_ty);
+    println!(
+        "sdk.allocate: {} {} -> {}\n",
+        elem_count, elem_ty, allocated_mem
+    );
+
     crate::debug_log!(format!(
         "sdk.allocate: {} {} -> {}\n",
         elem_count, elem_ty, allocated_mem
