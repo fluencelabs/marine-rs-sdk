75805fac0dd49705ac26e8130a2e57a88b8918bf
vms <michail.vms@gmail.com>
2021-05-10 14:34:49 +0300

    fix tests

--- fluence/tests/compilation_tests/export_functions/inner_vec_refs.rs
+++ fluence/tests/compilation_tests/export_functions/inner_vec_refs.rs
@@ -1,5 +1,9 @@
 #![allow(improper_ctypes)]
 
+use fluence::marine;
+
+pub fn main() {}
+
 #[marine]
 pub fn inner_arrays_2(_arg: &Vec<Vec<Vec<Vec<u8>>>>) -> &Vec<Vec<&Vec<Vec<u8>>>> {
     unimplemented!()
