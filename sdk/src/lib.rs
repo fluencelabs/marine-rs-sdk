ecce9a47e9e2ce49ea49b48a250a430a793c3d66
vms <michail.vms@gmail.com>
2021-06-07 11:16:10 +0300

    build fix

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -82,9 +82,9 @@ pub use call_parameters::SecurityTetraplet;
 pub use call_parameters::get_call_parameters;
 
 #[cfg(feature = "logger")]
-pub use fluence_sdk_main::WasmLoggerBuilder;
+pub use marine_rs_sdk_main::WasmLoggerBuilder;
 #[cfg(feature = "logger")]
-pub use fluence_sdk_main::TargetMap;
+pub use marine_rs_sdk_main::TargetMap;
 
 pub use mounted_binary::MountedBinaryResult;
 pub use mounted_binary::MountedBinaryStringResult;
