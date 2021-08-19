df2bfbdc517c5d4f309e777ffc79a46ab9e049d9
Valery Antopol <valery.antopol@gmail.com>
2021-08-19 20:44:02 +0300

    bump test sdk version

--- sdk-test/src/lib.rs
+++ sdk-test/src/lib.rs
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#![doc(html_root_url = "https://docs.rs/sdk-test/0.1.11")]
+#![doc(html_root_url = "https://docs.rs/sdk-test/0.2.0")]
 #![deny(
     dead_code,
     nonstandard_style,
