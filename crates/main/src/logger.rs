789e2b8e53c0844ef45e8e30f9e0f6352b83af7a
Valery Antopol <valery.antopol@gmail.com>
2024-02-07 16:26:16 +0400

    fix warnings

--- crates/main/src/logger.rs
+++ crates/main/src/logger.rs
@@ -238,6 +238,7 @@ pub fn log_utf8_string(level: i32, target: i32, msg_ptr: i32, msg_size: i32) {
     println!("[{}] {} {}", level, target, msg);
 }
 
+#[allow(unused_doc_comments)]
 /// TODO: mark `log_utf8_string_impl` as #[wasm_bindgen], so it is polyfilled by bindgen
 /// log_utf8_string should be provided directly by a host.
 #[cfg(all(feature = "marine-abi", target_arch = "wasm32"))]
